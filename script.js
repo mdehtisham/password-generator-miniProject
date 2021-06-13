2c0858fed273ff61f28b7db26801b5fb9c0d5020
Md. Ehtisham Farooqui <mefxe01@gmail.com>
2021-06-14 00:57:26 +0530

    added the logic and functionality to generate random password

--- script.js
+++ script.js
@@ -3,12 +3,55 @@
 // 65-90 for Uppercase A to Z.
 // 97-122 for Lowercase a to z.
 
+
+
+const restultEl = document.getElementById("result")
+const lengthEl = document.getElementById("length")
+const uppercaseEl = document.getElementById("uppercase")
+const lowecaseEl = document.getElementById("lowercase")
+const numberEl = document.getElementById("numbers")
+const symbolsEl = document.getElementById("symbols")
+const generateEl = document.getElementById("generate")
+const clipboardEl = document.getElementById("clipboard")
+
 const randomFunction = {
   lower: getRandomLower,
   upper: getRandomUpper,
   number: getRandomNumber,
   symbol: getRandomSymbol
 }
+
+
+
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value; // + will parse the string into number
+  const hasLower = lowecaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numberEl.checked;
+  const hasSymbols = symbolsEl.checked;
+
+  restultEl.innerHTML = generatePassword(hasLower, hasUpper, hasNumber, hasSymbols, length)
+
+})
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(v => Object.values(v)[0]);
+  // Object.values gives the array of values, in above case we only have one value in each object that is why we are using Object.values(v)[0]....and it return true or false, and on the basis of true and false filter function returns only those objects whose value is true.
+
+
+  if (typesCount === 0) return ''
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0]
+      generatedPassword += randomFunction[funcName]()
+    })
+  }
+  return generatedPassword;
+}
+
 
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97) // this will generate random Lowercase letters from a to z
@@ -29,4 +72,4 @@ function getRandomSymbol() {
 // console.log(getRandomLower())
 // console.log(getRandomUpper())
 // console.log(getRandomNumber())
-console.log(getRandomSymbol())
+// console.log(getRandomSymbol())
